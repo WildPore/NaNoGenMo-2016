1595da453161cc785b2ee97eae8b4fea2922faa0
Lewis Parnell <lewis.g.parnell@gmail.com>
2016-11-30 23:10:03 -0800

    Add final version of program

--- main.py
+++ main.py
@@ -4,15 +4,20 @@ def main():
     with open("prideandprejudice.txt") as f:
         text = f.read()
     text_model = markovify.Text(text)
-    for i in range(10):
-        print(text_model.make_sentence_with_start("Wedding"))
 
-def first_word(file):
-    first_words = []
-    with open(file) as f:
-        for line in f:
-            first_words.append(line.rstrip().split(' ', 1)[0])
+    novel = ''
+    words = 0
 
-    return first_words
+    while(words < 50000):
+        line = text_model.make_sentence_with_start("Mr. Darcy")
+        line = line.split()
+        words += len(line)
+
+        for word in line:
+            novel += ''.join(word) + ' '
+        novel += '\n'
+
+    with open("mrdarcyandmrdarcy.txt", "w") as f:
+        f.write(novel)
 
 main()
